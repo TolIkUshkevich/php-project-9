f5c7b3afdbc9d0b926380420b82efb1d19743488
Anatoliy Yushkevich <104827125+TolIkUshkevich@users.noreply.github.com>
2024-12-20 22:52:49 +0300

    fix linter

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ start:
 	PHP_CLI_SERVER_WORKERS=1 php -S 0.0.0.0:$(PORT) -t public
 
 lint:
-	composer exec --verbose phpstan
+	composer exec --verbose --memory-limit 256M phpstan analyse public
 
 install:
 	composer install
--- public/PsqlConnection.php
+++ public/PsqlConnection.php
@@ -11,20 +11,16 @@ class PsqlConnection
      */
     public function connect(): \PDO
     {
-        if (getenv('DATABASE_URL') === false) {
-            throw new \Exception("Error reading database configuration file");
-        } else {
-            $databaseUrl = parse_url(getenv('DATABASE_URL'));
-            $conStr = sprintf(
-                "pgsql:host=%s;dbname=%s;user=%s;password=%s",
-                $databaseUrl['host'],
-                ltrim($databaseUrl['path'], '/'),
-                $databaseUrl['user'],
-                $databaseUrl['pass']
-            );
-            $pdo = new \PDO($conStr);
-            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-        }
+        $databaseUrl = parse_url(getenv('DATABASE_URL'));
+        $conStr = sprintf(
+            "pgsql:host=%s;dbname=%s;user=%s;password=%s",
+            $databaseUrl['host'],
+            ltrim($databaseUrl['path'], '/'),
+            $databaseUrl['user'],
+            $databaseUrl['pass']
+        );
+        $pdo = new \PDO($conStr);
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $pdo;
     }
 }
